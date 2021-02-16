9e92119512833f469def4c0c9957e2521aad688b
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2021-02-16 13:48:54 +0200

    0.24.58-2

--- examples/useState.js
+++ examples/useState.js
@@ -2,14 +2,41 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [type, setType] = useState("users");
+  let [counter, setCounter] = useState(0);
+
+  const [state, setState] = useState({
+    title: "Counter",
+    date: Date.now()
+  });
+
+  function increment() {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+  function decrement() {
+    setCounter(counter - 1);
+    console.log(counter);
+  }
+
+  function updateTitle() {
+    setState((prev) => {
+      return { ...prev, title: "Новое название" };
+    });
+  }
 
   return (
     <div className="">
-      <h1> ресурс: {type}</h1>
-      <button onClick={() => setType("users")}>Users</button>
-      <button onClick={() => setType("todo")}>Todo</button>
-      <button onClick={() => setType("posts")}>Posts</button>
+      <h1>Счетчик: {counter}</h1>
+      <button onClick={increment} className="btn btn-success">
+        Добавить
+      </button>
+      <button onClick={decrement} className="btn btn-danger">
+        Убрать
+      </button>
+      <button onClick={updateTitle} className="btn">
+        Изменить название
+      </button>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
