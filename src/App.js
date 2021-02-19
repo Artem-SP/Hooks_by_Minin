5ab4942cd8fd479fef4aaa30f79431500d25a1e6
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2021-02-19 09:04:39 +0200

    192021 8:48 tm 0:59:35

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import React, { useState, useEffect, useRef, useMemo } from "react";
 import "./styles.css";
 
-function complexCompute(num) {
-  let i = 0;
-  while (i < 1000000001) i++;
-  return num * 2;
-}
-
 export default function App() {
-  const [number, setNumber] = useState(42);
-  const [colored, setColored] = useState(false);
+  const [count, setCount] = useState(false);
+  const [colored, setColored] = useState(1);
 
-  const styles = useMemo(() => {
-    return { color: colored ? "darkred" : "blue" };
-  }, [colored]);
-
-  const computed = useMemo(() => {
-    return complexCompute(number);
-  }, [number]);
-
-  useEffect(() => {
-    console.log("Styles changed");
-  }, [styles]);
+  const styles = {
+    color: colored ? "darkred" : "blue"
+  };
 
   return (
     <div className="">
-      <h1 style={styles}>Вычисляемое свойство: {number}</h1>
+      <h1 style={styles}>Количество элементов:</h1>
       <button
         className={"btn btn-success"}
-        onClick={() => setNumber((prev) => prev + 1)}
+        onClick={() => setCount((prev) => prev + 1)}
       >
         Добавить
-      </button>
-      <button
-        className={"btn btn-danger"}
-        onClick={() => setNumber((prev) => prev - 1)}
-      >
-        Убрать
       </button>
       <button
         className={"btn btn-warning"}
