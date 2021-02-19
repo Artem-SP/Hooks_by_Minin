af6f4a162875f04fe4e57f076320d0ccd8f5a2b9
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2021-02-19 13:44:14 +0200

    190221 13:43 tm 1:05:43

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef, useMemo } from "react";
+import ItemsList from "./itemsList";
 import "./styles.css";
 
 export default function App() {
@@ -9,9 +10,13 @@ export default function App() {
     color: colored ? "darkred" : "blue"
   };
 
+  const generateItemsFromAPI = () => {
+    return new Array(count).fill("").map((_, it) => `Item ${it + 1}`);
+  };
+
   return (
     <div className="">
-      <h1 style={styles}>Количество элементов:</h1>
+      <h1 style={styles}>Количество элементов: {count}</h1>
       <button
         className={"btn btn-success"}
         onClick={() => setCount((prev) => prev + 1)}
@@ -24,6 +29,7 @@ export default function App() {
       >
         Изменить
       </button>
+      <ItemsList getItems={generateItemsFromAPI} />
     </div>
   );
 }
