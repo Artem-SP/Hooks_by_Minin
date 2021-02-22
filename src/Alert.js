cc5c6cca3472fd27bd7df5725dcb6546777c2e19
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2021-02-22 13:45:19 +0200

    220221 13:43 tm 1:12:38

---
+++ src/Alert.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+extort default function Alert () {
+  return (
+    <div> className={'alert alert-danger'}>
+      <p> Это очень важное объявление</p>
+      </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
-import ItemsList from "./itemsList";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback
+} from "react";
 import "./styles.css";
+import Main from "./Main";
 
 export default function App() {
-  const [count, setCount] = useState(false);
-  const [colored, setColored] = useState(1);
-
-  const styles = {
-    color: colored ? "darkred" : "blue"
-  };
-
-  const generateItemsFromAPI = () => {
-    return new Array(count).fill("").map((_, it) => `Item ${it + 1}`);
-  };
-
   return (
-    <div className="">
-      <h1 style={styles}>Количество элементов: {count}</h1>
-      <button
-        className={"btn btn-success"}
-        onClick={() => setCount((prev) => prev + 1)}
-      >
-        Добавить
-      </button>
-      <button
-        className={"btn btn-warning"}
-        onClick={() => setColored((prev) => !prev)}
-      >
-        Изменить
-      </button>
-      <ItemsList getItems={generateItemsFromAPI} />
+    <div className={"container"}>
+      <Main />
     </div>
   );
 }
